aaec7ee0bf7607ea91959365d0f216f50a07ac14
youri18095 <yuma0315x@gmail.com>
2026-04-21 18:39:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -546,9 +546,10 @@ def hp_bar(damage_min, damage_max, hp):
 
     st.write(f"残りHP: {remain_min} ~ {remain_max}")
 
-hp_bar(damage1, damage2, hp)
+
     
 st.subheader("結果")
+hp_bar(damage1, damage2, hp)
 st.write(f"ダメージ: {damage1} ~ {damage2}")
 st.write(f"割合: {damage3}% ~ {damage4}%")
 st.write(f"相性倍率: {相性倍率}")
